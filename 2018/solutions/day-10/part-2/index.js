a67f9f4fd2aab3f26983e147dc42dd274ec7b2be
Eric Allen <decoy@internetalche.me>
2018-12-11 18:48:39 -0500

    solved day 10 part 2

--- 2018/solutions/day-10/part-2/index.js
+++ 2018/solutions/day-10/part-2/index.js
@@ -1,9 +1,9 @@
 const Solution = require('../../../utils/solution.class');
 
-const solvePuzzle = require('./part-1');
+const solvePuzzle = require('./part-2');
 
 const day = 10;
-const part = 1;
+const part = 2;
 
 const exampleData = [
   'position=< 9,  1> velocity=< 0,  2>',
@@ -41,6 +41,6 @@ const exampleData = [
 
 const puzzle = new Solution({ day, part });
 
-// puzzle.solve(solvePuzzle, exampleData, false, 3); // Test Data
+//puzzle.solve(solvePuzzle, exampleData, false, 3); // Test Data
 
 puzzle.solve(solvePuzzle); // Real Data
