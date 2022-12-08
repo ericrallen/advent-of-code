f417e1028e21d7f1b2a076ca9f35d4e72d1e66dd
Eric allen <eric.allen@datadoghq.com>
2022-12-07 20:11:12 -0500

    chore: some formatting updates

--- 2022/advent/src/days/day_six.rs
+++ 2022/advent/src/days/day_six.rs
@@ -6,7 +6,7 @@ const MESSAGE_BUFFER_LENGTH: usize = 14;
 fn check_for_unique_sequence(sequence: &str, size: usize) -> bool {
   let mut sequence_array: Vec<char> = sequence.chars().collect();
 
-  log!("{:?}", sequence_array);
+  log!("{:?}", sequence_array.join());
 
   sequence_array.sort();
   sequence_array.dedup();
@@ -19,16 +19,21 @@ fn get_sequence(stream: &String, start: usize, end: usize) -> Option<&str> {
 }
 
 fn find_end_of_sequence(stream: &String, length: usize) -> usize {
+  // check the first N characters before we start iterating
   let first_sequence = get_sequence(stream, 0, length).unwrap();
 
-  let mut sequence_end_index: usize = 0;
-
   if check_for_unique_sequence(first_sequence, length) {
-    return length;
+    length
   } else {
+    // we'll update this once we find the right ending index
+    // TODO: find a pattern that can make this `panic!()` if we
+    // never find a unique sequence
+    let mut sequence_end_index: usize = 0;
+
     for (index, _) in stream.chars().enumerate() {
       // skip first character since we already checked the first sequence
       if index > 0 {
+        // get the next sequence to check
         let test_sequence = get_sequence(stream, index, index + length).unwrap();
 
         if check_for_unique_sequence(test_sequence, length) {
@@ -38,29 +43,29 @@ fn find_end_of_sequence(stream: &String, length: usize) -> usize {
         }
       }
     }
-  }
 
-  return sequence_end_index;
+    sequence_end_index
+  }
 }
 
 fn part_one(input: String) -> String {
   let marker_end = find_end_of_sequence(&input, PACKET_MARKER_LENGTH);
 
-  return format!("{marker_end}");
+  format!("{marker_end}")
 }
 
 
 fn part_two(input: String) -> String {
   let marker_end = find_end_of_sequence(&input, MESSAGE_BUFFER_LENGTH);
 
-  return format!("{marker_end}");
+  format!("{marker_end}")
 }
 
 
 pub fn main(input: String, part: &str) -> String {
   match part {
-    PART_TWO_INDICATOR => return part_two(input),
-    _ => return part_one(input)
+    PART_TWO_INDICATOR => part_two(input),
+    _ => part_one(input)
   }
 }
 
@@ -80,7 +85,13 @@ mod test {
 
   #[test]
   fn day_six_part_one_extra() {
-    let inputs = ["bvwbjplbgvbhsrlpgdmjqwftvncz", "nppdvjthqldpwncqszvftbrmjlhg", "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"];
+    let inputs = [
+      "bvwbjplbgvbhsrlpgdmjqwftvncz",
+      "nppdvjthqldpwncqszvftbrmjlhg",
+      "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+      "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+    ];
+
     let outputs = ["5", "6", "10", "11"];
 
     for (index, input) in inputs.iter().enumerate() {
@@ -92,7 +103,14 @@ mod test {
 
   #[test]
   fn day_six_part_two() {
-    let inputs = ["mjqjpqmgbljsphdztnvjfqwrcgsmlb", "bvwbjplbgvbhsrlpgdmjqwftvncz", "nppdvjthqldpwncqszvftbrmjlhg", "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"];
+    let inputs = [
+      "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+      "bvwbjplbgvbhsrlpgdmjqwftvncz",
+      "nppdvjthqldpwncqszvftbrmjlhg",
+      "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+      "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+    ];
+
     let outputs = ["19", "23", "23", "29", "26"];
 
     for (index, input) in inputs.iter().enumerate() {
